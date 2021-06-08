b4c8b9300331f6909a7a99b31b87f72f40d4bd0b
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-06-08 16:56:23 +0100

    add dynamic class to svg base el

--- client/src/components/Designs/SvgBase.vue
+++ client/src/components/Designs/SvgBase.vue
@@ -1,7 +1,8 @@
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="h-5 w-5 cursor-pointer"
+    class="cursor-pointer"
+    :class="classes ? classes : 'h-5 w-5'"
     fill="none"
     viewBox="0 0 24 24"
     :stroke="svgColour ? svgColour : 'currentColor'"
@@ -19,6 +20,6 @@
 
 <script>
 export default {
-  props: ["svg", "svgColour"],
+  props: ["svg", "svgColour", "classes"],
 };
 </script>
