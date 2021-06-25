0c24e6ea4e76d0fd644091eec20321f037bfde8d
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-06-25 19:23:32 +0100

    changed animation on landing cards

--- client/src/components/LandingPage/MainLanding.vue
+++ client/src/components/LandingPage/MainLanding.vue
@@ -19,30 +19,24 @@
 
       <div class="w-full flex mt-10 sm:mt-4" v-if="showImages == 'favourites'">
         <img
-          data-aos="fade-up"
-          class="w-1/2 h-96"
+          class="elementSlideUp w-1/2 h-96"
           :src="require(`../../assets/latestDesigns/mug1.jpeg`)"
         />
 
         <img
-          data-aos="fade-up"
-          data-aos-delay="150"
-          class="w-1/2 h-96"
+          class="elementSlideUp2 w-1/2 h-96"
           :src="require(`../../assets/latestDesigns/misc1.jpeg`)"
         />
       </div>
 
       <div class="w-full flex mt-7" v-if="showImages == 'tableware'">
         <img
-          data-aos="fade-up"
-          class="w-1/2 h-96"
+          class="elementSlideUp w-1/2 h-96"
           :src="require(`../../assets/latestDesigns/dish1.jpeg`)"
         />
 
         <img
-          data-aos="fade-up"
-          data-aos-delay="150"
-          class="w-1/2 h-96"
+          class="elementSlideUp2 w-1/2 h-96"
           :src="require(`../../assets/latestDesigns/dish2.jpeg`)"
         />
       </div>
