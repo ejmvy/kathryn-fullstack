3acc972c5860c9e67393d3e1af7960bb5f1b2deb
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-06-25 19:23:45 +0100

    removed animation on contact banner text

--- client/src/components/LandingPage/ContactBan.vue
+++ client/src/components/LandingPage/ContactBan.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="flex flex-col items-center py-14 bg-green-white">
-    <h1
-      data-aos="fade-right"
-      data-aos-duration="1000"
-      class="text-2xl md:text-4xl font-bold"
-    >
-      Keep in Touch ?
-    </h1>
+    <h1 class="text-2xl md:text-4xl font-bold">Keep in Touch ?</h1>
     <div class="flex justify-between m-auto w-1/3 my-14">
       <div v-for="icon in logos" :key="icon">
         <img
