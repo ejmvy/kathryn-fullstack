f0a2ae67d819f963e1fdf7b0529e5f0723277f01
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-06-28 20:56:39 +0100

    updated contact form to add emailjs functionality

--- client/src/views/Contact.vue
+++ client/src/views/Contact.vue
@@ -7,34 +7,30 @@
     >
       <TitleDesign :title="titleText"></TitleDesign>
       <div class="flex justify-between mt-14 w-11/12">
-        <div class="flex flex-1 flex-col text-green-dark px-10">
+        <form
+          class="contact-form flex flex-1 flex-col text-green-dark px-10"
+          @submit.prevent="sendEmail"
+        >
           <div class="flex flex-col mb-20">
             <div class="flex flex-col text-left">
               <label class="uppercase text-xxs py-5">Name</label>
-              <input class="contactInput h-8" v-model="form.name" type="text" />
+              <input class="contactInput h-8" name="user_name" type="text" />
             </div>
             <div class="flex flex-col text-left">
               <label class="uppercase text-xxs py-5">Email</label>
-              <input
-                class="contactInput h-8"
-                v-model="form.email"
-                type="text"
-              />
+              <input class="contactInput h-8" name="user_email" type="email" />
             </div>
           </div>
           <div class="flex flex-col text-left">
             <label class="uppercase text-xxs pb-5">Message</label>
-            <textarea
-              class="contactInput h-24"
-              v-model="form.message"
-            ></textarea>
+            <textarea class="contactInput h-24" name="message"></textarea>
           </div>
           <div>
-            <button class="btn-green btn-lrg mt-10" @click="submitForm()">
+            <button class="btn-green btn-lrg mt-10" type="submit" value="Send">
               Submit
             </button>
           </div>
-        </div>
+        </form>
         <div class="hidden md:block md:w-2/5">
           <div
             class="relative w-11/12 h-full text-left text-sm left-5 pl-10 border-l-2 border-gray-300"
@@ -76,6 +72,7 @@
 import TitleDesign from "../components/Designs/TitleDesign.vue";
 import Header from "../components/LandingPage/Header.vue";
 import Footer from "../components/LandingPage/Footer.vue";
+import emailjs from "emailjs-com";
 export default {
   props: ["itemInfo"],
   data() {
@@ -83,16 +80,33 @@ export default {
       titleText: "We'd Love to Hear From You!",
       windowWidth: window.innerWidth,
       logos: ["icons/insta.png", "icons/fb.png", "icons/etsy.png"],
-      form: {
-        name: "",
-        email: "",
-        message: "",
-      },
     };
   },
+  // user_ujpmxVNNa20Fcol3xAcTS
   methods: {
-    submitForm() {
-      if (this.form.name && this.form.email && this.form.message) {
+    sendEmail: (e) => {
+      emailjs
+        .sendForm(
+          "service_8n1sza3",
+          "template_smnwcg8",
+          e.target,
+          "user_ujpmxVNNa20Fcol3xAcTS"
+        )
+        .then(
+          (result) => {
+            console.log("SUCCESS!", result.status, result.text);
+            // this.showNotification(true);
+            e.target.reset();
+          },
+          (error) => {
+            console.log(error);
+            // this.showNotification(false, error);
+          }
+        );
+      // https://www.dyspatch.io/resources/templates/
+    },
+    showNotification(success, error) {
+      if (success) {
         this.emitter.emit("showNotification", {
           state: true,
           message: "Message Sent",
@@ -101,7 +115,7 @@ export default {
         this.emitter.emit("showNotification", {
           state: false,
           title: "Ooops!",
-          message: "Some information is missing.",
+          message: `Error: ${error}`,
         });
       }
     },
