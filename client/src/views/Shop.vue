d20873cd993a0880c572eb8d24c03b53b532b338
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-07-06 20:40:20 +0100

    updated style for shop page, adding header and border

--- client/src/views/Shop.vue
+++ client/src/views/Shop.vue
@@ -5,12 +5,15 @@
       <h1 class="text-2xl sm:text-4xl mt-5 md:mt-10 mb-10 md:mb-0">
         Our Collection
       </h1>
-      <div class="w-11/12 mt-4 flex justify-between text-gray-dark">
+      <div
+        class="w-11/12 mt-4 md:pt-5 pb-3 flex justify-between text-gray-dark border-b border-gray-300"
+      >
         <div class="flex">
-          <p>Collections</p>
-          <div class="px-5">/</div>
+          <p class="fontRegular">Collections</p>
+          <div class="px-5 fontRegular">/</div>
           <select
-            class="bg-transparent pr-10 cursor-pointer"
+            class="fontRegular bg-transparent cursor-pointer"
+            :class="windowWidth > 700 ? 'pr-10' : 'pr-2'"
             v-model="categorySelected"
           >
             <option
@@ -22,10 +25,21 @@
             </option>
           </select>
         </div>
-        <p>{{ productData.length }} items</p>
+        <p class="fontRegular">{{ productData.length }} products</p>
       </div>
 
-      <div class="w-11/12 pt-0 md:pt-6 flex flex-wrap justify-center">
+      <div v-if="isLoading" class="lds-roller mt-24">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      <div v-else class="w-11/12 pt-0 md:pt-6 flex flex-wrap justify-center">
         <!-- <transition-group name="projects"> -->
         <div v-for="(item, idx) in getProductData" :key="idx">
           <ItemCard data-aos="fade-up" :displayItem="item"></ItemCard>
@@ -46,6 +60,7 @@ import axios from "axios";
 export default {
   data() {
     return {
+      isLoading: false,
       titleText: "Our Collection",
       windowWidth: window.innerWidth,
       productData: [],
@@ -56,6 +71,7 @@ export default {
     };
   },
   created() {
+    this.isLoading = true;
     const categoryParam = this.$route.params.category;
     if (categoryParam)
       this.categoryName =
@@ -68,6 +84,8 @@ export default {
           this.categoryName.slice(0, 1).toUpperCase() +
           this.categoryName.slice(1);
       }
+      this.isLoading = true;
+      this.isLoading = false;
       this.productData = data.data;
     });
   },
@@ -116,5 +134,91 @@ select:focus {
 .projects-leave-active {
   position: absolute;
   z-index: -1;
+}
+
+/* // Loading animation */
+.lds-roller {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-roller div {
+  animation: lds-roller 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  transform-origin: 40px 40px;
+}
+.lds-roller div:after {
+  content: " ";
+  display: block;
+  position: absolute;
+  width: 7px;
+  height: 7px;
+  border-radius: 50%;
+  background: #365a69;
+  margin: -4px 0 0 -4px;
+}
+.lds-roller div:nth-child(1) {
+  animation-delay: -0.036s;
+}
+.lds-roller div:nth-child(1):after {
+  top: 63px;
+  left: 63px;
+}
+.lds-roller div:nth-child(2) {
+  animation-delay: -0.072s;
+}
+.lds-roller div:nth-child(2):after {
+  top: 68px;
+  left: 56px;
+}
+.lds-roller div:nth-child(3) {
+  animation-delay: -0.108s;
+}
+.lds-roller div:nth-child(3):after {
+  top: 71px;
+  left: 48px;
+}
+.lds-roller div:nth-child(4) {
+  animation-delay: -0.144s;
+}
+.lds-roller div:nth-child(4):after {
+  top: 72px;
+  left: 40px;
+}
+.lds-roller div:nth-child(5) {
+  animation-delay: -0.18s;
+}
+.lds-roller div:nth-child(5):after {
+  top: 71px;
+  left: 32px;
+}
+.lds-roller div:nth-child(6) {
+  animation-delay: -0.216s;
+}
+.lds-roller div:nth-child(6):after {
+  top: 68px;
+  left: 24px;
+}
+.lds-roller div:nth-child(7) {
+  animation-delay: -0.252s;
+}
+.lds-roller div:nth-child(7):after {
+  top: 63px;
+  left: 17px;
+}
+.lds-roller div:nth-child(8) {
+  animation-delay: -0.288s;
+}
+.lds-roller div:nth-child(8):after {
+  top: 56px;
+  left: 12px;
+}
+@keyframes lds-roller {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
