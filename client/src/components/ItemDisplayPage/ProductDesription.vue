c1d9d47b1fdd69cc07db29a91b33bbc101fe712c
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-07-06 20:39:25 +0100

    changed size of text for prod desc

--- client/src/components/ItemDisplayPage/ProductDesription.vue
+++ client/src/components/ItemDisplayPage/ProductDesription.vue
@@ -5,22 +5,22 @@
     <div class="flex w-11/12 xl:w-3/4 mt-10">
       <div class="flex flex-1 flex-col text-left px-2">
         <div class="pb-5 md:flex md:items-center">
-          <label class="title1 text-xs pr-10">{{ itemInfo.name }}</label>
-          <p class="text-gray-dark">{{ itemInfo.description }}</p>
+          <p class="title1 text-xs pr-10">{{ itemInfo.name }}</p>
+          <p class="text-gray-dark text-sm">{{ itemInfo.description }}</p>
         </div>
         <div class="flex flex-col items-start sm:flex-row sm:items-center pb-5">
           <label class="title1 text-xs pr-10">Dimensions</label>
-          <p class="text-gray-dark">{{ itemInfo.dimensions }}</p>
+          <p class="text-gray-dark text-sm">{{ itemInfo.dimensions }}</p>
         </div>
         <div class="flex flex-col items-start sm:flex-row sm:items-center pb-5">
           <label class="title1 text-xs pr-10">Colors</label>
-          <p class="text-gray-dark">{{ itemInfo.colour }}</p>
+          <p class="text-gray-dark text-sm">{{ itemInfo.colour }}</p>
         </div>
       </div>
       <div class="flex flex-1 flex-col text-left px-2">
         <div class="md:flex md:flex-row md:items-center">
           <label class="title1 text-xs pr-10">Washing Instructions</label>
-          <p class="text-gray-dark">{{ itemInfo.washing }}</p>
+          <p class="text-gray-dark text-sm">{{ itemInfo.washing }}</p>
         </div>
       </div>
     </div>
